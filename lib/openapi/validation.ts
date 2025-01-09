417aabe78f5b3f4eb36f533e624d7a056caf453a
Stephen Ramthun <stephenramthun@gmail.com>
2025-01-09 21:51:52 +0100

    Disable tullete linting-regel

--- lib/openapi/validation.ts
+++ lib/openapi/validation.ts
@@ -2,5 +2,5 @@ import { OpenApiObjectSchema } from "@/lib/openapi/schema"
 import { OpenApiDoc } from "@/lib/openapi/types"
 
 export const validateSchema = (value: object): OpenApiDoc => {
-  return OpenApiObjectSchema.parse(value)
+  return OpenApiObjectSchema.parse(value) as unknown as OpenApiDoc
 }
