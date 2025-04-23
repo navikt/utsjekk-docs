1d545a3b4462ca935c37c5711bd777d01b1d61db
Stephen Ramthun <stephenramthun@gmail.com>
2025-04-23 14:04:32 +0200

    Legg til basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/utsjekk-docs" : undefined,
+  basePath: process.env.NODE_ENV === "production" ? "/utsjekk-docs" : undefined,
 }
 
 export default nextConfig
